d2c0d19cacbce1d1c12a0f01b8754242ff24aaff
Gerald <kapgerado05@gmail.com>
2022-10-05 14:21:46 +0300

    Done with JavaScript looping code along lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const wrIteCards = ["Guadalupe", "Ollie", "Aki"];
+
+// Thank you, ${wrapwriteCards}, for the wonderful surprise gift!
+
+function writeCards(wrIteCards) {
+    let repLy = []
+  for (let kads = 0; kads < wrIteCards.length; kads++) {
+    repLy.push(`Thank you, ${wrIteCards[kads]}, for the wonderful surprise gift!`)
+    debugger;
+  }
+
+  return repLy;
+}
+writeCards(wrIteCards); 
+
+// An arrow function that logs out descenting numbers
+
+const countDown = () => {
+    let form1 = 10 
+    while (form1 > -1 ){
+        console.log(form1--);
+    }
+}
